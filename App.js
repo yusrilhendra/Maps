47c91e549ad0d38c623f2c738efdd6239039ce38
yusrilhendra <36873997+yusrilhendra@users.noreply.github.com>
2018-05-27 21:22:45 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+export default class App extends Component {
+
+  state = {
+    region: {
+      latitude: -8.149407,
+      longitude: 115.216667,
+      latitudeDelta: 0.8922,
+      longitudeDelta: 0.8421,
+    },
+    markers : [
+      {
+        key:1,
+        latlng: {
+          latitude:-8.1129579,
+          longitude: 115.0784907
+        },
+        title: 'Masjid Al badr',
+        subtitle: ''
+      },
+      {
+        key:2,
+        latlng: {
+          latitude:-8.112788,
+          longitude: 115.0883613
+        },
+        title: 'Masjid Al Taubah',
+        subtitle: ''
+      },
+      {
+        key:3,
+        latlng: {
+          latitude:-8.1058547,
+          longitude: 115.0872373
+        },
+        title: 'Masjid Agung Jami Singaraja'
+      }
+      ,
+      {
+        key:4,
+        latlng: {
+          latitude:-8.1196461,
+          longitude: 115.0730218
+        },
+        title: 'Masjid Al Ashriy'
+      }
+   ]
+  };
+
+  render() {
+    return (
+      <View style={styles.contMain}>
+        <View style={styles.contHeader}>
+            <Text style={styles.textHeader}>
+              Letak Masjid di Bali, Singaraja
+            </Text>
+        </View>
+        <View style={styles.contMaps}>
+              <MapView
+                style={styles.map}
+                region={this.state.region}
+              >
+              {this.state.markers.map(mark => (
+              <Marker
+                  key = {mark.key}
+                  coordinate={mark.latlng}
+                  title={mark.title}
+                  description={mark.subtitle}
+                />
+              ))}
+              </MapView>
+        </View>
+        <View style={styles.contFooter}>
+           <Text style={styles.textFooter}> http://y.actived@gmail.com </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  contMain: {
+      flex : 1
+  },
+  contHeader: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  contMaps : {
+    flex : 10
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contFooter: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  textFooter: {
+    fontSize: 16,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+});
